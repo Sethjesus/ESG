fbaeb937ad12412a1208a16f604a8f72b418e443
Sethjesus <162777479+Sethjesus@users.noreply.github.com>
2025-03-04 16:31:47 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,22 @@
-const CACHE_NAME = "esg-pwa-cache-v1";
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("開啟快取");
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/manifest.json',
+                '/icon-192.png',
+                '/icon-512.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName !== CACHE_NAME)
-          .map((cacheName) => caches.delete(cacheName))
-      );
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
